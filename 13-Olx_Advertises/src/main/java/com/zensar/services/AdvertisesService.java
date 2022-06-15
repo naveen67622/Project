834ce1fdaef6eb9cb5450a248bf786c38157e72d
Naveen <104185866+naveen67622@users.noreply.github.com>
2022-06-15 09:25:02 +0530

    Add files via upload

---
+++ 13-Olx_Advertises/src/main/java/com/zensar/services/AdvertisesService.java
@@ -0,0 +1,33 @@
+package com.zensar.services;
+
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.zensar.bean.Advertises;
+
+@Service
+public interface AdvertisesService {
+	
+	
+
+	public Advertises newAdvertise(Advertises ad);
+
+	// To update existing advertise
+	public Advertises updateAdvertise(Advertises ad, Integer id);
+
+	// To read all the advertise posted by logged in user
+	public List<Advertises> getAllAdvertises();
+
+	// Reads specific advertisement posed by logged in user
+
+	// Deletes specific advertisement posted by logged in user
+	public boolean deleteAdvertiseById(Integer id);
+
+	// Search advertisements based upon given filter criteria
+	// Matches advertisements using the provided 'searchText' within all fields
+	// of an advertise.
+	public Advertises getAdvertiseByText(String text);
+
+	// Get Advertise by id
+	public Advertises getAdvertisesById(Integer id);
+}
